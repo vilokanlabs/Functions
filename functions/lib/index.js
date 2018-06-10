dcb96e694ad408c4dc004c64bc4447bed0f9a87a
Pulkit Singh <pulkitsingh01@gmail.com>
2018-06-10 17:14:22 +0530

    ..

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,10 +1,9 @@
-import * as functions from 'firebase-functions';
-import admin from 'firebase-admin'
-admin.initializeApp()
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+//# sourceMappingURL=index.js.map
--- functions/src/index.js
+++ functions/src/index.js
@@ -1,5 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var firebase_admin_1 = __importDefault(require("firebase-admin"));
+firebase_admin_1.default.initializeApp();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
