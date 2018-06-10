2e0c3a08db29e9913407b448aa3eadfafa296084
Vilokan Labs <vilokanlabs@gmail.com>
2018-06-10 18:37:11 +0530

    firebase | deployed (but useless)

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,13 +1,25 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var firebase_admin_1 = __importDefault(require("firebase-admin"));
-firebase_admin_1.default.initializeApp();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+/**
+ * Responds to any HTTP request that can provide a "message" field in the body.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.helloWorld = function (req, res) {
+    if (req.body.message === undefined) {
+        // This is an error case, as "message" is required
+        res.status(400).send('No message defined!');
+    }
+    else {
+        // Everything is ok
+        console.log(req.body.message);
+        res.status(200).end();
+    }
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,4 @@
-import * as functions from 'firebase-functions';
-import admin from 'firebase-admin'
-admin.initializeApp()
+import { Request, Response } from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -8,3 +6,20 @@ admin.initializeApp()
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+/**
+ * Responds to any HTTP request that can provide a "message" field in the body.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.helloWorld = (req: Request, res: Response) => {
+    if (req.body.message === undefined) {
+      // This is an error case, as "message" is required
+      res.status(400).send('No message defined!');
+    } else {
+      // Everything is ok
+      console.log(req.body.message);
+      res.status(200).end();
+    }
+  };
